90beff741c278bdfdd056e1c7f5bbff2e782dc42
Mahdi Gholami <99770517+MhdiGholami@users.noreply.github.com>
2024-12-06 11:13:14 +0330

    Developed by Mahdi Gholami

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+﻿// Custom ContextMenu Project
+// ******************************
+
+
+let contextMenu = document.getElementById('contextMenuId')
+
+// Function for show context menu
+function contextMenuHandler(event) {
+  event.preventDefault()
+
+  contextMenu.style.top = event.pageY + 'px'
+  contextMenu.style.left = event.pageX + 'px'
+  contextMenu.style.display = 'block'
+}
+
+// Function for click to hide context menu
+function clickHandler() {
+  contextMenu.style.display = 'none'
+}
